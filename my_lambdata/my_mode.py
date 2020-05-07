31a16b5df5d5bb32dc2da76e91ac8b83fad3b251
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-05-08 03:42:11 +0900

    Move scripts to folder

---
+++ my_lambdata/my_mode.py
@@ -0,0 +1,9 @@
+# function
+def enlarge(n):
+    return n * 100
+
+if __name__ == "__main__":
+    # only run the code below if executing this script from command line
+    # otherwise don't runt it
+    y = int(input("please choose a number: "))
+    print(enlarge(y))
--- my_mode.py
+++
@@ -1,7 +0,0 @@
-# function
-def enlarge(n):
-    return n * 100
-
-
-y = int(input("please choose a number: "))
-print(enlarge(y))
