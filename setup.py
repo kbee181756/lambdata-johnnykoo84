92ed81ea76965a618cf9f184bee6f9eedac49255
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-05-08 03:44:27 +0900

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-johnnykoo",
-    version='0.1.0',
+    version='0.1.1',
     description="My first python package practice",
     long_description="Hello World, this is my very first python package!",
     long_description_content_type="text/markdown",
