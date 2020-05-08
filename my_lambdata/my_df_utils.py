320c8f5f8d6fb5bff636f883be27c05c86d89df8
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-05-09 08:13:44 +0900

    Refactor with OOP style

--- my_lambdata/my_df_utils.py
+++ my_lambdata/my_df_utils.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
+class MyUtil:
 
-def check_null(df):
-    return df.isnull().sum()
+    def __init__(self):
+        pass
 
+    def check_null(df):
+        return df.isnull().sum()
 
-def get_confusion_mtx(y_actu, y_pred):
-    """
-    #Returns confusion matrix
+    def get_confusion_mtx(y_actu, y_pred):
+        """
+        #Returns confusion matrix from pd Series
 
-    #Parameters:
-    #   y_actual (pandas series) e.g. y_actual
-    #   y_pred (pandas series) e.g. y_pred
+        #Parameters:
+        #   y_actual (pandas series) e.g. y_actual
+        #   y_pred (pandas series) e.g. y_pred
 
-    #Returns:
-    #   confusion matrix
-    """
-    return pd.crosstab(y_actu, y_pred)
+        #Returns:
+        #   confusion matrix
+        """
+
+        return pd.crosstab(y_actu, y_pred)
