2c56cecec5884166a5f185b212a25774b9cd7d2c
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-05-08 15:26:50 +0900

    Add more fns in df util helper

--- my_lambdata/my_df_utils.py
+++ my_lambdata/my_df_utils.py
@@ -2,3 +2,16 @@ import pandas as pd
 
 def check_null(df):
     return df.isnull().sum()
+
+def get_confusion_mtx(y_actu, y_pred):
+    """
+    #Returns confusion matrix
+
+    #Parameters:
+    #   y_actual (pandas series) e.g. y_actual
+    #   y_pred (pandas series) e.g. y_pred
+
+    #Returns:
+    #   confusion matrix
+    """
+    return pd.crosstab(y_actu, y_pred)
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-df = pd.DataFrame({"a": [1,2,3], "b": [4,5,6]})
+from my_df_utils import check_null, get_confusion_mtx
 
-print(df.head())
+# df = pd.DataFrame({"a": [1,2,3], "b": [4,5,6]})
+
+# print(df.head())
+
+df = pd.DataFrame({"a": [1, None, 3], "b": [4, 5, None], "c": [6, 7, 8], "d": [9, 10, 11]})
+
+y_actu = pd.Series([2, 0, 2, 2, 0, 1, 1, 2, 2, 0, 1, 2], name='Actual')
+y_pred = pd.Series([0, 0, 2, 1, 0, 2, 1, 0, 2, 0, 2, 2], name='Prediction')
+
+print(check_null(df))
+print(get_confusion_mtx(y_actu, y_pred))
